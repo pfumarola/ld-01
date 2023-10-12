6ecf9b12ed399904cc0a4ccc6898d4509dfeb050
pfumarola <paolo96fumarola@gmail.com>
2023-10-12 18:58:14 +0200

    Completed the Transaction model

--- database/models/transaction.go
+++ database/models/transaction.go
@@ -1,5 +1,12 @@
 package models
 
+import "time"
+
 type Transaction struct {
-	TransactionID uint `gorm:"primary_key"`
+	TransactionID uint      `json:"authorID" gorm:"primary_key"`
+	CustomerID    uint      `json:"customerID"`
+	BookID        uint      `json:"bookID"`
+	LoanDate      time.Time `json:"loanDate"`
+	DueDate       time.Time `json:"dueDate"`
+	ActualReturn  time.Time `json:"actualReturn"`
 }
