e3a2cabaa8435e22dddf6f31a7add68c5d218c0e
pfumarola <paolo96fumarola@gmail.com>
2023-10-12 19:12:16 +0200

    Adjusted ISBN length

--- database/models/book.go
+++ database/models/book.go
@@ -1,7 +1,7 @@
 package models
 
 type Book struct {
-	ISBN              string `gorm:"primary_key; size:13"`
+	ISBN              string `gorm:"primary_key; size:14"`
 	Title             string `json:"title" gorm:"size:255"`
 	AuthorID          uint   `json:"authorID"`
 	YearOfPublication string `json:"yearOfPublication" gorm:"size:4"`
