8fe528194f26dff74bacee41ee17ea2e565300eb
pfumarola <paolo96fumarola@gmail.com>
2023-10-13 14:38:35 +0200

    Implemented gorm relationship

--- database/models/book.go
+++ database/models/book.go
@@ -4,6 +4,7 @@ type Book struct {
 	ISBN              string `gorm:"primary_key; size:14"`
 	Title             string `json:"title" gorm:"size:255"`
 	AuthorID          uint   `json:"authorID"`
+	Author            Author `gorm:"references:AuthorID"`
 	YearOfPublication string `json:"yearOfPublication" gorm:"size:4"`
 	AvailableCopies   uint   `json:"availableCopies"`
 	TotalCopies       uint   `json:"totalCopies"`
--- database/models/transaction.go
+++ database/models/transaction.go
@@ -3,9 +3,11 @@ package models
 import "time"
 
 type Transaction struct {
-	TransactionID uint      `json:"authorID" gorm:"primary_key"`
+	TransactionID uint      `json:"transactionID" gorm:"primary_key"`
 	CustomerID    uint      `json:"customerID"`
+	Customer      Customer  `gorm:"references:CustomerID"`
 	BookID        uint      `json:"bookID"`
+	Book          Book      `gorm:"references:ISBN"`
 	LoanDate      time.Time `json:"loanDate"`
 	DueDate       time.Time `json:"dueDate"`
 	ActualReturn  time.Time `json:"actualReturn"`
